9dd4a1ba33a0cf0c96892fbf3aa100ba2cdf8cda
Phuc <phuc0200366@huce.edu.vn>
2023-11-26 21:46:55 +0700

    sửa link admin-panel(thêm page

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -85,11 +85,11 @@ background-image: linear-gradient(326deg, #861657 0%, #7b403b 74%);
     <div class="sidebar">
     <a href="admin_home.php?page=admin_users">Quản lý Thành viên</a>
     <a href="admin_home.php?page=admin_comments">Quản lý Góp ý</a>
-    <a href="admin_home.php?admin_book_table.php">Quản lý Đặt bàn</a>
-    <a href="admin_home.php?admin_blogs.php">Quản lý Blog</a>
-    <a href="admin_home.php?admin_foods.php">Quản lý Món ăn</a>
-    <a href="admin_home.php?admin_categories.php">Quản lý Danh mục</a>
-    <a href="admin_home.php?admin_orders.php">Quản lý Đơn hàng</a>
+    <a href="admin_home.php?page=admin_book_table">Quản lý Đặt bàn</a>
+    <a href="admin_home.php?page=admin_blogs">Quản lý Blog</a>
+    <a href="admin_home.php?page=admin_foods">Quản lý Món ăn</a>
+    <a href="admin_home.php?page=admin_categories">Quản lý Danh mục</a>
+    <a href="admin_home.php?page=admin_orders">Quản lý Đơn hàng</a>
     </div>
 
     <div class="admin-panel">
