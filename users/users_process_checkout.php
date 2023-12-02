330a9bbf5cb218266cb1cf7cae37fffece3ab063
Phuc <phuc0200366@huce.edu.vn>
2023-12-02 15:13:59 +0700

    sửa thông báo

--- users/users_process_checkout.php
+++ users/users_process_checkout.php
@@ -52,13 +52,14 @@ foreach ($_SESSION['cart_item'] as $item) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
 <div class="alert alert-success" role="alert">
-        <h4 class="alert-heading">Well done!</h4>
+        <h4 class="alert-heading">Bạn đặt thành công òi!</h4>
         <p><?php echo $_SESSION['success_message']; ?></p>
         <hr>
-        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+        <p class="mb-0">Đơn hàng của bạn sẽ được chuyển về trang giỏ hàng sau 5 giây.</p>
     </div>
 </body>
   <script>
@@ -66,4 +67,5 @@ foreach ($_SESSION['cart_item'] as $item) {
             window.location.href = "users_cart.php";
         }, 5000);
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
